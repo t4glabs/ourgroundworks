3e82ce7652bddee9f3454d31849b100161e0cb77
jinsoraj <jinsoraj2000@gmail.com>
2023-01-19 11:08:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var hamburger = document.getElementById("drop")
+
+// var ham=document.getElementById("icon")
+// var cross=document.getElementById("cross")
+
+cross.style.display="none"
+function myFunction() {
+    if (hamburger.style.display == "block") {
+   
+
+        hamburger.style.display = "none";
+    }
+    else {
+        hamburger.style.display = "block";
+  
+
+    }
+}
+
+
+
+
+
